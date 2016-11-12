7e8d8aaa78889df3f01d6f5cef73c142be64c4ad
Richard Hanulewicz <rshanule@buffalo.edu>
2016-11-11 22:57:35 -0500

    Design launched!

---
+++ script.js
@@ -0,0 +1,25 @@
+function _(element){
+	return document.getElementById(element);
+}
+
+function pack(){
+	_("therest").style.left = _("sidething").offsetWidth + 32;
+	_("propic").height = _("propic").offsetWidth;
+	_("dividyguy").style.marginRight = -(window.innerWidth - _("sidething").offsetWidth - _("titletext").offsetWidth - 49);
+}
+
+pack();
+window.onresize = function(event) {pack();};
+
+$("#slice1,#slice2,#slice3,#slice4, #sliceL").mouseenter(function(){
+	slice = $(this).attr("id");
+	pslice = "#p" + slice;
+	$(pslice).animate({left: '15%'}, 50); 	
+	$(pslice).clearQueue();
+});
+
+$("#slice1,#slice2,#slice3,#slice4, #sliceL").mouseleave(function(){
+	slice = $(this).attr("id")
+	pslice = "#p" + slice;
+	$(pslice).animate({left: '10%'}, 100); 
+});
